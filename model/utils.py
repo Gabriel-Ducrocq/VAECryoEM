909074a53b208abab32ea70fb3a4c88ddf325e6d
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2023-11-01 13:56:38 +0100

    trying to train the pose only first on berzelius

--- model/utils.py
+++ model/utils.py
@@ -89,6 +89,13 @@ def parse_yaml(path):
               latent_type=experiment_settings["latent_type"], latent_dim=experiment_settings["latent_dimension"])
     vae.to(device)
 
+
+    if experiment_settings["train_pose_only"]:
+        for name, p in vae.named_parameters():
+            if ("weight" in name or "bias" in name) and ("encoder" in name or "decoder" in name) and ("encoder_pose" not in name
+            and "decoder_pose" not in name):
+                print(f"Disabling gradient for {name}")
+
     pixels_x = np.linspace(image_settings["image_lower_bounds"][0], image_settings["image_upper_bounds"][0],
                            num=image_settings["N_pixels_per_axis"][0]).reshape(1, -1)
 
