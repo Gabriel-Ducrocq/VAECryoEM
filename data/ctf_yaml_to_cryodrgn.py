7725084666be3477d3040f9ff7f8e192162abb2e
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2023-12-21 15:13:37 +0100

    gain

---
+++ data/ctf_yaml_to_cryodrgn.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pickle
+import yaml
+
+parser_arg = argparse.ArgumentParser()
+parser_arg.add_argument('--yaml', type=str, required=True)
+parser_arg.add_argument('--pkl', type=str, required=True)
+parser_arg.add_argument('--Nimages', type=int, required=True)
+
+
+with open(experiment_settings["image_yaml"], "r") as file:
+	image_settings = yaml.safe_load(file)
+
+ctf_params = np.zeros((args.Nimages, 9))
+ctf_params[:, 0] = image_settings["N_pixels_per_axis"][0]
+ctf_params[:, 1] = (image_settings["image_upper_bounds"][0] - image_settings["image_lower_bounds"][0])/image_settings["N_pixels_per_axis"][0]
+    fields = (
+        "ctf/df1_A",
+        "ctf/df2_A",
+        "ctf/df_angle_rad",
+        "ctf/accel_kv",
+        "ctf/cs_mm",
+        "ctf/amp_contrast",
+        "ctf/phase_shift_rad",
+    )
+
+ctf_params[:, 2] = image_settings["dfU"]
+ctf_params[:, 3] = image_settings["dfV"]
+ctf_params[:, 4] = image_settings["dfang"]
+ctf_params[:, 5] = image_settings["accelerating_voltage"]
+ctf_params[:, 6] = image_settings["spherical_aberration"]
+ctf_params[:, 7] = image_settings["amplitude_contrast_ratio"]
+ctf_params[:, 8] = 0
+
+with open(args.pkl, "wb") as f:
+	pickle.dump(ctf_params.astype(np.float32), f)
+
