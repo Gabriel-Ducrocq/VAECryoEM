57b9adcae639902108853d424fccc8523aded8df
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2023-12-21 15:20:02 +0100

    again

--- data/ctf_yaml_to_cryodrgn.py
+++ data/ctf_yaml_to_cryodrgn.py
@@ -15,12 +15,12 @@ with open(args.yaml, "r") as file:
 ctf_params = np.zeros((args.Nimages, 9))
 ctf_params[:, 0] = image_settings["N_pixels_per_axis"][0]
 ctf_params[:, 1] = (image_settings["image_upper_bounds"][0] - image_settings["image_lower_bounds"][0])/image_settings["N_pixels_per_axis"][0]
-ctf_params[:, 2] = image_settings["dfU"]
-ctf_params[:, 3] = image_settings["dfV"]
-ctf_params[:, 4] = image_settings["dfang"]
-ctf_params[:, 5] = image_settings["accelerating_voltage"]
-ctf_params[:, 6] = image_settings["spherical_aberration"]
-ctf_params[:, 7] = image_settings["amplitude_contrast_ratio"]
+ctf_params[:, 2] = image_settings["rendered"]["dfU"]
+ctf_params[:, 3] = image_settings["rendered"]["dfV"]
+ctf_params[:, 4] = image_settings["rendered"]["dfang"]
+ctf_params[:, 5] = image_settings["rendered"]["accelerating_voltage"]
+ctf_params[:, 6] = image_settings["rendered"]["spherical_aberration"]
+ctf_params[:, 7] = image_settings["rendered"]["amplitude_contrast_ratio"]
 ctf_params[:, 8] = 0
 
 with open(args.pkl, "wb") as f:
