99cb694fb084c0e84a3d42a5de75d22ec1dae878
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-03-01 17:39:48 +0100

    again

--- data/analysis.py
+++ data/analysis.py
@@ -110,7 +110,7 @@ all_translation_per_domain = []
 all_axis_angle_per_domain = []
 
 
-"""
+
 ### BE CAREFUL !!!! ADDED A START !!!!
 #start = step*5000
 start = 0
@@ -130,7 +130,7 @@ for i, batch_images in tqdm(enumerate(images)):
     quaternions_per_domain, translations_per_domain = vae.decode(latent_variables)
     rotation_per_residue = utils.compute_rotations_per_residue(quaternions_per_domain, mask, device)
     translation_per_residue = utils.compute_translations_per_residue(translations_per_domain, mask)
-
+    """
     predicted_structures = utils.deform_structure(gmm_repr.mus, translation_per_residue,
                                                        rotation_per_residue)
 
@@ -148,7 +148,8 @@ for i, batch_images in tqdm(enumerate(images)):
             latent_type=experiment_settings["latent_type"], volume=True)
 
         mrc.write(f"{folder_output}volume_{i+start}.mrc", np.transpose(batch_predicted_volumes[0].detach().cpu().numpy(), axes=(2, 1, 0)), Apix=1.0, is_vol=True)
-
+    """
+    
     all_latent_mean.append(latent_mean.to("cpu"))
     all_latent_std.append(latent_std.to("cpu"))
     np.save(f"{folder_output}all_rotations_per_residue_{i}.npy", rotation_per_residue.to("cpu").detach().numpy())
@@ -165,7 +166,6 @@ for i, batch_images in tqdm(enumerate(images)):
 #all_translation_per_residue = concat_and_save(all_translation_per_residue, f"{folder_output}all_translation_per_residue.npy")
 all_latent_mean = concat_and_save(all_latent_mean, f"{folder_output}all_latent_mean.npy")
 all_latent_std = concat_and_save(all_latent_std, f"{folder_output}all_latent_std.npy")
-"""
 #all_rotations_per_domain = concat_and_save(all_axis_angle_per_domain, f"{folder_experiment}all_rotations_per_domain.npy")
 #all_translation_per_domain = concat_and_save(all_translation_per_domain, f"{folder_experiment}all_translation_per_domain.npy")
 #print("REGISTERED !")
