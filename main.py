0a779aa0cfbc5cd0af3ce9369631dfd33314a419
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-03-02 23:13:42 +0100

    again

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ def train(yaml_setting_path, debug_mode):
             batch_poses = batch_poses.to(device)
             batch_poses_translation = batch_poses_translation.to(device)
             indexes = indexes.to(device)
-            plt.imshow(batch_images[0].detach().cpu())
-            plt.show()
+            #plt.imshow(batch_images[0].detach().cpu())
+            #plt.show()
             latent_variables, latent_mean, latent_std = vae.sample_latent(batch_images)
 
             mask = vae.sample_mask(batch_size)
