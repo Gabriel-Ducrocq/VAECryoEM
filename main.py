4846632205068d73b4e8b431b809009238214d13
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-03-03 10:20:24 +0100

    again

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 import wandb
 import argparse
 import model.utils
+import numpy as np
 from time import time
 from model import renderer
 from model.loss import compute_loss
@@ -50,6 +51,7 @@ def train(yaml_setting_path, debug_mode):
         data_loader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=True))
         for batch_num, (indexes, batch_images, batch_poses, batch_poses_translation) in enumerate(data_loader):
             start = time()
+            np.save(batch_images[0].detach().numpy(), "data/dataset/cryoStar_test/image.npy")
             batch_images = batch_images.to(device)
             batch_poses = batch_poses.to(device)
             batch_poses_translation = batch_poses_translation.to(device)
