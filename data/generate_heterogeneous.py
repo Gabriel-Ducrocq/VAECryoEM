0c87088880e203630545c3b50eb62b42979e0529
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-03-04 13:49:20 +0100

    again

--- data/generate_heterogeneous.py
+++ data/generate_heterogeneous.py
@@ -114,6 +114,7 @@ all_images = []
 from time import time
 
 if is_homogeneous:
+    print("Using the target structure:", target_structure_path)
     backbone = torch.tensor(target_structure.coord - center_vector, dtype=torch.float32, device=device)
     backbone = torch.concatenate([backbone[None, :, :] for _ in range(N_pose_per_structure)], dim=0)
 
