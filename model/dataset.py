614697e364f2d79d17bcf322da40f4b47990fc71
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-03-23 13:50:58 +0100

    trying to read the mrcs on the file

--- model/dataset.py
+++ model/dataset.py
@@ -1,29 +1,32 @@
+import os
 import torch
 import numpy as np
 from torch.utils.data import Dataset
 from pytorch3d.transforms import euler_angles_to_matrix
 
 class ImageDataSet(Dataset):
-    def __init__(self, images, particles_df):
+    def __init__(self, images, particles_df, particles_path):
         """
         Create a dataset of images and poses
         :param images: torch.tensor(N_images, N_pix_x, N_pix_y) of images
-        :param poses: pandas dataframe (N_images, undefined) of innformation regarding each particles, in Relion star format.
+        :param poses: pandas dataframe (N_images, undefined) of information regarding each particles, in Relion star format.
+        :param particles_path: str, path to the data folder containing the mrcs files.
         """
+        self.particles_path = particles_path
         euler_angles_degrees = particles_df[["rlnAngleRot", "rlnAngleTilt", "rlnAnglePsi"]].values
         euler_angles_radians = euler_angles_degrees*np.pi/180
         poses = euler_angles_to_matrix(torch.tensor(euler_angles_radians, dtype=torch.float32), convention="ZYZ")
         poses = torch.transpose(poses, dim0=-2, dim1=-1)
         poses_translation = torch.zeros((particles_df.shape[0], 3), dtype=torch.float32)
         poses_translation[:, :2] = torch.tensor(particles_df[["rlnOriginX", "rlnOriginY"]].values, dtype=torch.float32)
-        assert images.shape[0] == poses.shape[0] and images.shape[0] == poses_translation.shape[0]
+        #assert images.shape[0] == poses.shape[0] and images.shape[0] == poses_translation.shape[0]
         assert torch.max(torch.abs(poses_translation)) == 0, "Only 0 translation supported as poses"
-        print("Dataset size:", images.shape)
-        self.images = torch.flatten(images, start_dim=-2, end_dim=-1)
-        self.avg_image = torch.mean(self.images, dim=0, keepdim=True)
-        self.std_image = torch.std(self.images, dim=0, keepdim=True)
+        #print("Dataset size:", images.shape)
+        #self.images = torch.flatten(images, start_dim=-2, end_dim=-1)
+        #self.avg_image = torch.mean(self.images, dim=0, keepdim=True)
+        #self.std_image = torch.std(self.images, dim=0, keepdim=True)
         print("Normalizing training data")
-        self.images = (self.images - self.avg_image)/self.std_image
+        #self.images = (self.images - self.avg_image)/self.std_image
         self.poses = poses
         self.poses_translation = poses_translation
 
@@ -35,4 +38,21 @@ class ImageDataSet(Dataset):
         return self.images.shape[0]
 
     def __getitem__(self, idx):
-        return idx, self.images[idx], self.poses[idx], self.poses_translation[idx]
+        particles = self.particles_df.iloc[idx]
+        try:
+            mrc_idx, img_name = particles["rlnImageName"].split("@")
+            mrc_idx = int(mrc_idx) - 1
+            mrc_path = os.path.join(self.particles_path, img_name)
+            with mrcfile.mmap(mrc_path, mode="r", permissive=True) as mrc:
+                if mrc.data.ndim > 2:
+                    proj = torch.from_numpy(np.array(mrc.data[in_mrc_idx])).float() * self.cfg.scale_images
+                else:
+                    # the mrcs file can contain only one particle
+                    proj = torch.from_numpy(np.array(mrc.data)).float() * self.cfg.scale_images
+
+        except Exception as e:
+            print(f"WARNING: Particle image {img_name_raw} invalid! Setting to zeros.")
+            print(e)
+            proj = torch.zeros(1, self.down_side_shape, self.down_side_shape)
+
+        return idx, proj, self.poses[idx], self.poses_translation[idx]
