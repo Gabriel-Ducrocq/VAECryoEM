1e7ac183e993b4481610a67070db546aae8f89c2
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-03-25 13:28:39 +0100

    again

--- data/generate_compositional_heterogeneous.py
+++ data/generate_compositional_heterogeneous.py
@@ -168,11 +168,11 @@ for i in tqdm(range(n_iter)):
         backbone = torch.tensor(backbone, dtype=torch.float32, device=device)
         backbone = torch.concatenate([backbone[None, :, :] for _ in range(N_pose_per_structure)], dim=0)
 
-    if i in indexes_to_replace:
+    if i+1 in indexes_to_replace:
         batch_images = renderer_mising.compute_x_y_values_all_atoms(backbone, poses[i*N_pose_per_structure:(i+1)*N_pose_per_structure], poses_translation[i*N_pose_per_structure:(i+1)*N_pose_per_structure])
     else:
         batch_images = renderer.compute_x_y_values_all_atoms(backbone, poses[i*N_pose_per_structure:(i+1)*N_pose_per_structure], poses_translation[i*N_pose_per_structure:(i+1)*N_pose_per_structure])
-        
+
     plt.imshow(batch_images[0].detach().cpu().numpy())
     plt.show()
     all_images.append(batch_images.detach().cpu())
