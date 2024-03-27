771fc90aaedac15c691a8729d2f36e82d9f55ae3
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-03-27 10:08:02 +0100

    again

--- data/analysis.py
+++ data/analysis.py
@@ -47,7 +47,7 @@ filter_aa = True
 
 
 
-def analyze(yaml_setting_path, model_path, latent_path, structures_path):
+def analyze(yaml_setting_path, model_path, latent_path, structures_path, z):
     """
     train a VAE network
     :param yaml_setting_path: str, path the yaml containing all the details of the experiment
@@ -58,45 +58,69 @@ def analyze(yaml_setting_path, model_path, latent_path, structures_path):
     vae.eval()
     all_latent_variables = []
     data_loader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=False))
-    for batch_num, (indexes, batch_images, batch_poses, batch_poses_translation) in enumerate(data_loader):
-        start = time()
-        batch_images = batch_images.to(device)
-        batch_poses = batch_poses.to(device)
-        batch_poses_translation = batch_poses_translation.to(device)
-        indexes = indexes.to(device)
+    if z is None:
+        for batch_num, (indexes, batch_images, batch_poses, batch_poses_translation) in enumerate(data_loader):
+            start = time()
+            batch_images = batch_images.to(device)
+            batch_poses = batch_poses.to(device)
+            batch_poses_translation = batch_poses_translation.to(device)
+            indexes = indexes.to(device)
 
-        start_net = time()
-        latent_variables, latent_mean, latent_std = vae.sample_latent(batch_images)
-        all_latent_variables.append(latent_variables)
-        mask = vae.sample_mask(batch_images.shape[0])
-        quaternions_per_domain, translations_per_domain = vae.decode(latent_variables)
-        start_old = time()
-        #rotation_per_residue = model.utils.compute_rotations_per_residue(quaternions_per_domain, mask, device)
-        end_old = time()
-        start_new = time()
-        rotation_per_residue = utils.compute_rotations_per_residue_einops(quaternions_per_domain, mask, device)
-        end_new = time()
-        translation_per_residue = utils.compute_translations_per_residue(translations_per_domain, mask)
-        end_net = time()
-        print("Net time:", end_net - start_net)
-        start_deforming = time()
-        predicted_structures = utils.deform_structure(gmm_repr.mus, translation_per_residue,
-                                                           rotation_per_residue)
+            start_net = time()
+            latent_variables, latent_mean, latent_std = vae.sample_latent(batch_images)
+            all_latent_variables.append(latent_variables)
+            """
+            mask = vae.sample_mask(batch_images.shape[0])
+            quaternions_per_domain, translations_per_domain = vae.decode(latent_variables)
+            start_old = time()
+            #rotation_per_residue = model.utils.compute_rotations_per_residue(quaternions_per_domain, mask, device)
+            end_old = time()
+            start_new = time()
+            rotation_per_residue = utils.compute_rotations_per_residue_einops(quaternions_per_domain, mask, device)
+            end_new = time()
+            translation_per_residue = utils.compute_translations_per_residue(translations_per_domain, mask)
+            end_net = time()
+            print("Net time:", end_net - start_net)
+            start_deforming = time()
+            predicted_structures = utils.deform_structure(gmm_repr.mus, translation_per_residue,
+                                                               rotation_per_residue)
 
-        for i, pred_struct in enumerate(predicted_structures):
+            for i, pred_struct in enumerate(predicted_structures):
+                base_structure.coord = pred_struct.detach().cpu().numpy()
+                base_structure.to_pdb(os.path.join(structures_path, f"structure_{batch_num*batch_size + i}.pdb"))
+
+
+
+            end_deforming = time()
+            print("Deforming time", end_deforming - start_deforming)
+
+            end = time()
+            print("Iteration duration:", end-start)
+            """
+
+        all_latent_variables = torch.concat(all_latent_variables, dim=0).detach().cpu().numpy()
+        np.save(latent_path, all_latent_variables)
+    else:
+        z = np.load(z)
+        for i, latent_variables in enumerate(z):
+            latent_variables = latent_variables[None, :]
+            mask = vae.sample_mask(latent_variables.shape[0])
+            quaternions_per_domain, translations_per_domain = vae.decode(latent_variables)
+            start_old = time()
+            #rotation_per_residue = model.utils.compute_rotations_per_residue(quaternions_per_domain, mask, device)
+            end_old = time()
+            start_new = time()
+            rotation_per_residue = utils.compute_rotations_per_residue_einops(quaternions_per_domain, mask, device)
+            end_new = time()
+            translation_per_residue = utils.compute_translations_per_residue(translations_per_domain, mask)
+            end_net = time()
+            print("Net time:", end_net - start_net)
+            start_deforming = time()
+            predicted_structures = utils.deform_structure(gmm_repr.mus, translation_per_residue,
+                                                               rotation_per_residue)
+
             base_structure.coord = pred_struct.detach().cpu().numpy()
-            base_structure.to_pdb(os.path.join(structures_path, f"structure_{batch_num*batch_size + i}.pdb"))
-
-
-
-        end_deforming = time()
-        print("Deforming time", end_deforming - start_deforming)
-
-        end = time()
-        print("Iteration duration:", end-start)
-
-    all_latent_variables = torch.concat(all_latent_variables, dim=0).detach().cpu().numpy()
-    np.save(latent_path, all_latent_variables)
+            base_structure.to_pdb(os.path.join(structures_path, f"structure_z_{i}.pdb"))
 
 
 
@@ -108,12 +132,14 @@ if __name__ == '__main__':
     parser_arg.add_argument("--model", type=str, required=True)
     parser_arg.add_argument("--latent_path", type=str, required=True)
     parser_arg.add_argument("--structures_path", type=str, required=True)
+    parser_arg.add_argument("--z", type=str, required=False)
     args = parser_arg.parse_args()
     structures_path = args.structures_path
     latent_path = args.latent_path
     model_path = args.model
     path = args.experiment_yaml
-    analyze(path, model_path, latent_path, structures_path)
+    z = args.z
+    analyze(path, model_path, latent_path, structures_path, z)
 
 
 
