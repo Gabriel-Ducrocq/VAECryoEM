39035f20deabdd557f631f62a1f90b80f98bbdc3
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-04-08 10:27:18 +0200

    testing 0 translation

--- main.py
+++ main.py
@@ -111,6 +111,11 @@ def train(yaml_setting_path, debug_mode):
             end_loss = time()
             #print("Loss time", end_loss - start_loss)
             print("Epoch:",  epoch, "Batch number:", batch_num, "Loss:", loss, "device:", device)
+            test = torch.sum(torch.abs(batch_poses_translation))
+            print("Batch trans", test)
+            if test != 0:
+                print("PROBLEM !")
+                break
             start_gradient = time()
             loss.backward()
             optimizer.step()
