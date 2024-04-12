fc0c0f0378e48ca4fc73715c90cda1c33f8d1ecf
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-04-12 18:16:46 +0200

    again

--- data/generate_homogeneous_eman2.py
+++ data/generate_homogeneous_eman2.py
@@ -1,5 +1,6 @@
 import time
 import argparse
+import numpy as np
 from EMAN2 import *
 from tqdm import tqdm
 from scipy.spatial.transform import Rotation
@@ -18,9 +19,13 @@ all_rotations_eman2 = all_rotations.as_euler("ZXZ", degrees=True)
 all_rotations_relion = all_rotations.as_euler("ZYZ", degrees=True)
 
 start = time.time()
+all_images = []
 for rot in tqdm(all_rotations_eman2):
 	t = Transform({"type":"eman","az":rot[0],"alt":rot[1], "phi":rot[2]})
 	proj = vol.project("standard", t)
+	image = np.flip(proj.numpy(), axis=0)
+	all_images.append(image)
 
+all_images = np.stack(all_images, axis=0)
 end = time.time()
 print(end-start)
