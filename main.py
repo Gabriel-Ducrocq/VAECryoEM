4f20059ad2b22da1114e378ac2e85745728b9aaf
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-04-14 23:43:56 +0200

    again

--- main.py
+++ main.py
@@ -130,9 +130,11 @@ def train(yaml_setting_path, debug_mode):
             end = time()
             print("Iteration duration:", end-start)
 
-
-        end_tot = time()
-        print("TOTAL TIME", end_tot - start_tot) 
+            if batch_num == 100
+            end_tot = time()
+            print("TOTAL TIME", end_tot - start_tot) 
+            break
+            
         break 
 
         
