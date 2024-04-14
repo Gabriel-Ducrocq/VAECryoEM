a3f7e4caf4520e9a52ded558bdd88fc415c52202
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-04-14 23:25:13 +0200

    again

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def train(yaml_setting_path, debug_mode):
         tracking_metrics = {"rmsd":[], "kl_prior_latent":[], "kl_prior_mask_mean":[], "kl_prior_mask_std":[],
                             "kl_prior_mask_proportions":[], "l2_pen":[]}
 
-        data_loader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers = 2))
+        data_loader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers = 4))
         start_tot = time()
         for batch_num, (indexes, batch_images, batch_poses, batch_poses_translation) in enumerate(data_loader):
             start = time()
