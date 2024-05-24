b9c13fda1914b36d959fed37fe954d78673f8a3f
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-05-24 17:29:52 +0200

    again

--- data/structure_to_volume.py
+++ data/structure_to_volume.py
@@ -46,7 +46,7 @@ def structure_to_volume(image_yaml, structure_path, output_path):
     volume = renderer.structure_to_volume(gmm_repr.mus[None, :,:], gmm_repr.sigmas, gmm_repr.amplitudes, grid, device)
     end = time.time()
     print(f"Time to generate a volume on {Npix} a side again2:", end-start)
-        start = time.time()
+    start = time.time()
     volume = renderer.structure_to_volume(gmm_repr.mus[None, :,:], gmm_repr.sigmas, gmm_repr.amplitudes, grid, device)
     end = time.time()
     print(f"Time to generate a volume on {Npix} a side again3:", end-start)
