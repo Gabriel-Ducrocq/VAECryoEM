9e718c0acd90c1fba50f851d50246976658684f8
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-06-05 13:42:59 +0200

    again

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def train(yaml_setting_path, debug_mode):
             print("Time real", end_real - start_real)
             print("Percentage of freqs kept:", np.mean((torch.sqrt(torch.sum(ctf.freqs**2, dim=1)) < 1/4.0).numpy()))
             print("Number of freqs kept:", np.sum((torch.sqrt(torch.sum(ctf.freqs**2, dim=1)) < 1/4.0).numpy()))
-            freqs_proj = torch.fft.fftshift(torch.fft.fftfreq(dataset.down_side_shape, dataset.down_apix))[:57]
+            freqs_proj = torch.fft.fftshift(torch.fft.fftfreq(dataset.down_side_shape, dataset.down_apix, device=device))[:57]
             start_fourier = time()
             predicted_images_fourier = renderer.project_fourier(posed_predicted_structures, gmm_repr.sigmas, gmm_repr.amplitudes, freqs_proj)
             end_fourier = time()
