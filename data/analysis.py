f71eef3a1fe7b862ff4a38bbd1f9c58e9ddbccc3
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-06-21 11:58:52 +0200

    again

--- data/analysis.py
+++ data/analysis.py
@@ -119,11 +119,11 @@ def analyze(yaml_setting_path, model_path, latent_path, structures_path, z):
             #latent_variables = latent_variables[None, :]
             #latent_var = torch.zeros((batch_size, z.shape[1]), device=device)
             #latent_var[:z.shape[0]] = z
-            mask = vae.sample_mask(z.shape[0])
+            segments = vae.sample_mask(z.shape[0])
             quaternions_per_domain, translations_per_domain = vae.decode(z)
             #rotation_per_residue = model.utils.compute_rotations_per_residue(quaternions_per_domain, mask, device)
             #rotation_per_residue = utils.compute_rotations_per_residue_einops(quaternions_per_domain, mask, device)
-            translation_per_residue = utils.compute_translations_per_residue(translations_per_domain, mask)
+            translation_per_residue = utils.compute_translations_per_residue(translations_per_domain, segments)
             predicted_structures = utils.deform_structure_bis(gmm_repr.mus, translation_per_residue, quaternions_per_domain, segments, device)
             #predicted_structures = utils.deform_structure(gmm_repr.mus, translation_per_residue,
             #                                                   rotation_per_residue)
