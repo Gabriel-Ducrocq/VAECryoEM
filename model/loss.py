ca8886580a740cba5bc5bdd291c45e48d218b906
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-07-05 11:46:01 +0200

    investigating slow code

--- model/loss.py
+++ model/loss.py
@@ -147,7 +147,8 @@ def compute_loss(predicted_images, images, mask_image, latent_mean, latent_std, 
         vae.mask_parameters, experiment_settings["mask_prior"],
         "means", epsilon_kl=experiment_settings["epsilon_kl"])
 
-    continuity_loss = compute_continuity_loss(predicted_structures, true_structure, device)
+    #continuity_loss = compute_continuity_loss(predicted_structures, true_structure, device)
+    continuity_loss = 0
     KL_prior_mask_stds = compute_KL_prior_mask(vae.mask_parameters, experiment_settings["mask_prior"],
                                                "stds", epsilon_kl=experiment_settings["epsilon_kl"])
     KL_prior_mask_proportions = compute_KL_prior_mask(vae.mask_parameters, experiment_settings["mask_prior"],
