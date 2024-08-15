729956f309c7dc41dfb061e2f70933bdf4cc4250
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-08-15 21:00:06 +0200

    thinning to get only 10k structures

--- data/analysis_integral.py
+++ data/analysis_integral.py
@@ -154,7 +154,7 @@ def analyze(yaml_setting_path, model_path, structures_path, z, thinning=1, dimen
 
     else:
             print(all_latent_variables)
-            all_latent_variables= torch.tensor(all_latent_variables, dtype=torch.float32, device=device)
+            all_latent_variables= torch.tensor(all_latent_variables[::thinning], dtype=torch.float32, device=device)
             latent_variables_loader = iter(DataLoader(all_latent_variables, shuffle=False, batch_size=batch_size))
             for batch_num, z in enumerate(latent_variables_loader):  
                 print("Batch number:", batch_num)
