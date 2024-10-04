b557995c4235fbd708b776f81635ab9d86baebf6
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-10-04 11:55:29 +0700

    filtering the atom array to keep only the amino acids

--- data/generate_full_structures.py
+++ data/generate_full_structures.py
@@ -20,6 +20,7 @@ from polymer import Polymer
 from Bio.PDB import PDBParser
 import matplotlib.pyplot as plt
 from dataset import ImageDataSet
+import biotite.structure as struc
 from gmm import Gaussian, EMAN2Grid
 from sklearn.decomposition import PCA
 from torch.utils.data import DataLoader
@@ -117,6 +118,7 @@ def analyze(yaml_setting_path, model_path, structures_path, z, thinning=1, dimen
     all_number_of_atoms = []
     f = PDBFile.read(experiment_settings["base_structure_path"])
     atom_arr_stack = f.get_structure()
+    atom_arr_stack = atom_arr_stack[struc.filter_amino_acids(atom_arr_stack)]
     base_coordinates = torch.tensor(atom_arr_stack.coord, dtype=torch.float32, device=device)
     for chain_id in np.unique(atom_arr_stack.chain_id):
         chain = atom_arr_stack[:, atom_arr_stack.chain_id == chain_id]
