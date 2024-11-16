b1733c521206e3b90eecaa037403dfaae3dd110b
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-11-16 23:10:41 +0700

    generating an even lower SNR dataset

--- data/generate_heterogeneous.py
+++ data/generate_heterogeneous.py
@@ -164,9 +164,7 @@ for i in tqdm(range(n_iter)):
     posed_backbones = rotate_structure(backbone, poses[i*N_pose_per_structure:(i+1)*N_pose_per_structure])
     batch_images = project(posed_backbones, torch.ones((backbone.shape[1], 1), device=device)*sigma_gmm, amplitudes, grid)
 
-    ########## I DEACTIVATE CTF CORRUPTION !!!!!! ######
-    #batch_ctf_corrupted_images = apply_ctf(batch_images, ctf, torch.tensor([j for j in range(i*N_pose_per_structure, (i+1)*N_pose_per_structure)], device=device))
-    batch_ctf_corrupted_images = batch_images
+    batch_ctf_corrupted_images = apply_ctf(batch_images, ctf, torch.tensor([j for j in range(i*N_pose_per_structure, (i+1)*N_pose_per_structure)], device=device))
     ###  !!!!!!!!!!!!! We multiply by -1 so that when we correct for the translation in the cryoSPHERE run, we dont get 2x translation but 0 tranlations !
     batch_poses_translation = - poses_translations[i*N_pose_per_structure:(i+1)*N_pose_per_structure]
     batch_translated_images = image_translator.transform(batch_ctf_corrupted_images, batch_poses_translation[:, None, :])
@@ -200,8 +198,7 @@ print("Mean variance accross images", mean_variance)
 print("Adding Gaussian noise with variance", noise_var)
 torch.save(all_images, f"{folder_experiment}ImageDataSetNoNoise")
 
-############# I DEACTIVATE THE NOISE CORRUPTION !!!!!!!!
-#all_images += torch.randn((N_images, Npix, Npix))*torch.sqrt(noise_var)
+all_images += torch.randn((N_images, Npix, Npix))*torch.sqrt(noise_var)
 print("Saving images in MRC format")
 print(size_prot)
 if len(size_prot) > 1:
