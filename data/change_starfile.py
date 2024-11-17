e12e518b431fad6960dbacc18b920dd2ee3f7249
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-11-17 15:34:30 +0700

    adding the file to change the starfile

---
+++ data/change_starfile.py
@@ -0,0 +1,24 @@
+import argparse
+import starfile
+
+def change_mrcs_path(x, new_path):
+	image_number, _ = x.split("@")
+	return image_number + "@" + new_path
+
+def change_starfile(starfile_path, new_image_path, new_starfile_path):
+	df = starfile.read(starfile_path)
+	df["particles"]["rlnImageName"] = df["particles"]["rlnImageName"].apply(lamdba x: starfile_path(x, new_image_path))
+	starfile.save(new_starfile_path, df)
+
+
+
+if __name__ == '__main__':
+    parser_arg = argparse.ArgumentParser()
+    parser_arg.add_argument('--starfile', type=str, required=True)
+    parser_arg.add_argument('--new_image_path', type=str, required=True)
+    parser_arg.add_argument('--new_starfile_path', type=str, required=True)
+    args = parser_arg.parse_args()
+    starfile = args.stafile
+    new_image_path = args.new_image_path
+    new_starfile_path = args.new_starfile_path
+    change_starfile(starfile_path, new_image_path, new_starfile_path)
