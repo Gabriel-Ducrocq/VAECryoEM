7e24bfa58ecc4b0b6fc19a1128820e5850adacb3
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-11-20 01:46:32 +0700

    again

--- data/get_mask.py
+++ data/get_mask.py
@@ -42,7 +42,7 @@ segments = vae.sample_mask(1)
 hard_segments = np.argmax(segments.detach().cpu().numpy(), axis=-1)
 all_segments = []
 for l in range(vae.N_domains):
-	print(hard_segments.shape)
+	print(np.sum(hard_segments[0] == l))
 	all_segments.append(np.sum(hard_segments[0] == l))
 
 
