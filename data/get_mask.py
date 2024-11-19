9d4e801d3013eed112f3ff7422858ac4e55399f5
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-11-20 01:36:01 +0700

    simplifying the coloration

---
+++ data/get_mask.py
@@ -0,0 +1,43 @@
+import sys
+import os
+path = os.path.abspath("model")
+sys.path.append(path)
+import mrc
+import yaml
+import torch
+import utils
+import mrcfile
+import argparse
+import starfile
+import numpy as np
+from ctf import CTF
+import seaborn as sns
+from time import time
+from tqdm import tqdm
+import Bio.PDB as bpdb
+from Bio.PDB import PDBIO
+from polymer import Polymer
+from Bio.PDB import PDBParser
+import matplotlib.pyplot as plt
+from dataset import ImageDataSet
+from gmm import Gaussian, EMAN2Grid
+from sklearn.decomposition import PCA
+from torch.utils.data import DataLoader
+from scipy.spatial.distance import cdist
+from pytorch3d.transforms import quaternion_to_axis_angle, quaternion_to_matrix
+
+
+
+
+
+
+
+parser_arg = argparse.ArgumentParser()
+parser_arg.add_argument('--model_path', type=str, required=True)
+args = parser_arg.parse_args()
+module_path = args.module_path
+vae = torch.load(model_path)
+segments = vae.sample_mask(1)
+hard_segments = np.argmax(segments.detach().cpu().numpy(), axis=-1)
+print(hard_segments)
+
