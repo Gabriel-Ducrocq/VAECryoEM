b7f5a389b7eabf64bf422cc08194a7456a4ecc2b
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-11-20 01:49:43 +0700

    again

--- data/get_mask.py
+++ data/get_mask.py
@@ -46,7 +46,10 @@ for l in range(vae.N_domains):
 
 
 ll = np.cumsum(all_segments)
+list_coloring = []
 for i in range(len(ll)-1):
-    print(f"color #1:{ll[i]}-{ll[i+1]} {cols[i]}")  
+    list_coloring.append(f"color #1:{ll[i]}-{ll[i+1]} {cols[i]}")
+
+ print(" ".join(list_coloring))  
 
 
