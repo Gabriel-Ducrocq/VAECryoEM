248da3d7e3fcf1d6af381100ec2e310f1737e324
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-11-22 12:05:35 +0700

    restoring the ctf

--- model/renderer.py
+++ model/renderer.py
@@ -192,9 +192,7 @@ def apply_ctf(images, ctf, indexes):
 
     ##### !!!!!!!!!!!!!!!! I AM MULTIPLYING BY -1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     fourier_images = primal_to_fourier2d(images)
-    #### I DEACTIVATE THE CTFFFFFFF !!!!!! ##########
-    #fourier_images *= -ctf.compute_ctf(indexes)
-    fourier_images *= -1
+    fourier_images *= -ctf.compute_ctf(indexes)
     ctf_corrupted = fourier2d_to_primal(fourier_images)
     return ctf_corrupted
 
