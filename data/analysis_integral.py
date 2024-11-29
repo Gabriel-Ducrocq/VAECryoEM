41e5aa70f50356e94d095e0485a44ca9568b1675
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2024-11-29 12:02:10 +0100

    again

--- data/analysis_integral.py
+++ data/analysis_integral.py
@@ -101,7 +101,7 @@ def analyze(yaml_setting_path, model_path, structures_path, z, thinning=1, dimen
     vae.eval()
     all_latent_variables = []
     data_loader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4))
-    if z is None:
+    if z is None and not generate_images:
         for batch_num, (indexes, batch_images, batch_poses, batch_poses_translation, _) in enumerate(data_loader):
             print("Batch number:", batch_num)
             print("dimensions", dimensions)
