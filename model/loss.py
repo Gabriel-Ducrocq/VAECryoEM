6d16907004b7e8df601abece45207a5e6d562dde
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-04-13 14:19:36 +0200

    fixing bug in loss function

--- model/loss.py
+++ model/loss.py
@@ -164,9 +164,9 @@ def compute_loss(predicted_images, images, predicted_rotation_matrix_pose, batch
     #angles = torch.mean(angles).detach().cpu().numpy()
     print("Angles:", angles)
 
-    rotmat_metric = roma.rigid_vectors_registration(viewpoint_predicted, viewpoint_true)
-    rotate_viewpoint_predicted = torch.einsum("ik, bk -> bi", rotmat_metric, viewpoint_predicted)
-    diff_viewpoints = torch.mean(torch.sqrt(torch.sum((rotate_viewpoint_predicted - viewpoint_true)**2, axis=-1)))
+    #rotmat_metric = roma.rigid_vectors_registration(viewpoint_predicted, viewpoint_true)
+    #rotate_viewpoint_predicted = torch.einsum("ik, bk -> bi", rotmat_metric, viewpoint_predicted)
+    #diff_viewpoints = torch.mean(torch.sqrt(torch.sum((rotate_viewpoint_predicted - viewpoint_true)**2, axis=-1)))
     tracking_dict["viewpoint_angle_diff_degrees"].append(angles.detach().cpu().numpy())
 
     loss = rmsd + loss_weights["KL_prior_latent"]*KL_prior_latent \
