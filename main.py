71e6793ab678fb5d00e0571a66a4f35ffc37a061
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-04-13 08:12:29 +0200

    again

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ def train(yaml_setting_path, debug_mode):
         tracking_metrics = {"rmsd":[], "kl_prior_latent":[], "kl_prior_mask_mean":[], "kl_prior_mask_std":[],
                             "kl_prior_mask_proportions":[], "l2_pen":[], "viewpoint_angle_diff_degrees":[]}
 
+
         #### !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DROP LAST !!!!!! ##################################
         data_loader = tqdm(iter(DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers = 4, drop_last=True)))
         start_tot = time()
