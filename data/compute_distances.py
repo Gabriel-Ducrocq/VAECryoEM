f156656110e181f1d35ecf39680134bb4832e530
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-05-20 11:30:27 +0200

    accelerating the computation of the distances with numba

--- data/compute_distances.py
+++ data/compute_distances.py
@@ -12,6 +12,7 @@ from ctf import CTF
 import numpy as np
 from tqdm import tqdm
 from polymer import Polymer
+from numba import njit, prange
 
 parser_arg = argparse.ArgumentParser()
 parser_arg.add_argument('--path', type=str, required=True)
@@ -21,33 +22,35 @@ path = args.path
 chain_information = args.chain_information
 
 
-def compute_distance(polymer, idxA, idxB, chain_id):
+def compute_distance(polymer, idxA, idxB, chain_id, idx):
     """ Computes the distance between the two upper domains"""
     domainA = polymer.coord[np.isin(polymer.res_id, idxA) & (polymer.chain_id == chain_id[0])]
     domainB = polymer.coord[np.isin(polymer.res_id, idxB) & (polymer.chain_id == chain_id[1])]
     center_a = np.mean(domainA, axis=0)
     center_b = np.mean(domainB, axis=0)
-    return np.sqrt(np.sum((center_a - center_b)**2))
+    return np.sqrt(np.sum((center_a - center_b)**2)), idx
 
-def compute_distance_no_chain(polymer, idxA, idxB):
+def compute_distance_no_chain(polymer, idxA, idxB, idx):
     """Compute the distances between two domains when there is no chain information"""
     domainA = polymer.coord[np.isin(polymer.res_id, idxA)]
     domainB = polymer.coord[np.isin(polymer.res_id, idxB)]
     center_a = np.mean(domainA, axis=0)
     center_b = np.mean(domainB, axis=0)
-    return np.sqrt(np.sum((center_a - center_b)**2))
+    return np.sqrt(np.sum((center_a - center_b)**2)), idx
 
+@njit(parallel=True)
 def compute_distribution_distances(path, idxA, idxB, predicted=False, chain_id = ["A", "B"], chain_information=True):
     """ Computes the distances for all the structures present in the folder path """
     all_distances = []
+    all_indexes = []
     start = 1
-    end = 20001
+    end = 10001
     if predicted:
         start = 0
-        end = 20000
+        end = 10000
         
     res_ids = [i for i in range(1, 504)]
-    for i in range(start, end):
+    for i in prange(start, end):
         if not predicted:
             try:
                 path_struct = path + "test_"+str(i)+ ".pdb"
@@ -69,11 +72,10 @@ def compute_distribution_distances(path, idxA, idxB, predicted=False, chain_id =
                 pol = Polymer.from_pdb(path_struct)
             
         if chain_information:
-            dist = compute_distance(pol, idxA, idxB, chain_id)
+            dist = compute_distance(pol, idxA, idxB, chain_id, i)
         else:
-            dist = compute_distance_no_chain(pol, idxA, idxB)
+            dist = compute_distance_no_chain(pol, idxA, idxB, i)
 
-        print(dist)
         all_distances.append(dist)
         
     return np.array(all_distances)
