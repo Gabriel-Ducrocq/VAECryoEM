d574d8278fb7d4532105536cc9487cafaadd748f
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-07-30 09:13:05 +0200

    fixing the projection

--- data/generate_trash_particles.py
+++ data/generate_trash_particles.py
@@ -170,7 +170,7 @@ if not noise_only:
 
         amplitudes = torch.tensor(poly.num_electron, dtype=torch.float32, device=device)[:, None]
         posed_backbones = rotate_structure(backbone, poses[i*N_pose_per_structure:(i+1)*N_pose_per_structure])
-        batch_images = project(posed_backbones, torch.ones((backbone.shape[1], 1), device=device)*sigma_gmm, amplitudes, grid)
+        batch_images = project(posed_backbones, torch.ones((backbone.shape[1], 1), device=device)*sigma_gmm, amplitudes[initial_index:ending_index], grid)
 
         batch_ctf_corrupted_images = apply_ctf(batch_images, ctf, torch.tensor([j for j in range(i*N_pose_per_structure, (i+1)*N_pose_per_structure)], device=device))
         ###  !!!!!!!!!!!!! We multiply by -1 so that when we correct for the translation in the cryoSPHERE run, we dont get 2x translation but 0 tranlations !
