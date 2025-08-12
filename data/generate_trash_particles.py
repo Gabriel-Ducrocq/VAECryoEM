c8d82860a4061f5a636574d958101df1f9901573
gabriel-ducrocq <gabrielducrocq.maths@gmail.com>
2025-08-12 11:25:56 +0200

    again

--- data/generate_trash_particles.py
+++ data/generate_trash_particles.py
@@ -171,6 +171,7 @@ if not noise_only:
         amplitudes = torch.tensor(poly.num_electron, dtype=torch.float32, device=device)[:, None]
         posed_backbones = rotate_structure(backbone, poses[i*N_pose_per_structure:(i+1)*N_pose_per_structure])
         print("AMPLITUDES", amplitudes.shape)
+        print("BACKBONE SHAPE", backbone.shape)
         print("INIT", initial_index, "END", ending_index)
         batch_images = project(posed_backbones, torch.ones((backbone.shape[1], 1), device=device)*sigma_gmm, amplitudes[initial_index:ending_index], grid)
 
